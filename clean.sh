9851c916a530822ec3d9cc276a9473097e366d69
ralfluebben <ralfluebben>
2008-09-21 17:16:51 +0000

    *** empty log message ***

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 make clean
 find ./ -name CVS -exec rm -rf {} \;
-find ./ -name *~ -exec rm -rf {} \;
-find ./ -name *kdevelop* -exec rm -rf {} \;
+find ./ -name "*~" -exec rm -rf {} \;
+find ./ -name "*kdevelop*" -exec rm -rf {} \;
+rm -rf radiusplugin_v2.0a 
 rm main
 
