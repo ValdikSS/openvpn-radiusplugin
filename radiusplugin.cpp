ValdikSS/openvpn-radiusplugin

commit 3f6a9a0595089d143622e7bdba3facc1b725532a

Fix for openvpn process fault state while cpu high loaded
With high loaded cpu (a lot of client connections, tunnel
traffic, etc) after a while we got situation with a lot of
unmanaged sockets (stucked in close_wait state) and with no ability
to manage new connections. This fix removes deadlock wich leads to
described fault state.

radiusplugin.cpp
@@ -429,6 +429,8 @@ error:
                 cerr << getTime() << "Unknown Exception!";
 
             }
+            pthread_mutex_unlock (context->getMutexSend());
+            pthread_mutex_unlock (context->getMutexRecv());
             return OPENVPN_PLUGIN_FUNC_ERROR;
             /////////////////////////// CLIENT_CONNECT
         }
@@ -910,7 +912,9 @@ void  * auth_user_pass_verify(void * c)
             if ( DEBUG ( context->getVerbosity() ) ) cerr << getTime() << "RADIUS-PLUGIN: FOREGROUND THREAD: Waiting for new user." << endl;
             cout.flush();
 	    pthread_mutex_lock(context->getMutexSend());
-            pthread_cond_wait(context->getCondSend(),context->getMutexSend());
+            while(context->UserWaitingtoAuth() == false) {
+                pthread_cond_wait(context->getCondSend(),context->getMutexSend());
+            }
 	    pthread_mutex_unlock(context->getMutexSend());
         }
         if (context->getStopThread()==true)
@@ -1098,6 +1102,7 @@ void  * auth_user_pass_verify(void * c)
             delete newuser;
         }
     }
+    pthread_mutex_unlock(context->getMutexRecv());
     pthread_mutex_unlock(context->getMutexSend());
     cerr << getTime() << "RADIUS-PLUGIN: FOREGROUND THREAD: Thread finished.\n";
     pthread_exit(NULL);

AcctScheduler.cpp
@@ -151,11 +151,17 @@ void AcctScheduler::doAccounting(PluginContext * context)
 	iter1=activeuserlist.begin();
 	iter2=activeuserlist.end();
 		
-	
+	time_t start_time;
+	time(&start_time);
 	while (iter1!=iter2)
 	{
 		//get the time
 		time(&t);
+
+		if((t - start_time) > iter1->second.getAcctInterimInterval()) {
+			cerr << getTime() << "Update ticket loop interrupted (to avoid progressive delay)\n";
+			return;
+		}
 		//if the user needs an update
 		if ( t>=iter1->second.getNextUpdate())
 		{

commit 7729aaf08b2340321d1049d01855ffbfdccf895a

Remove auth_user_pass_verify error introduced by commit 3f6a9a0595089d143622e7bdba3facc1b725532a
We should not check for UserWaitingtoAuth() because that would prevent thread from graceful termination.

radiusplugin.cpp
@@ -863,9 +863,7 @@ void  * auth_user_pass_verify(void * c)
             if ( DEBUG ( context->getVerbosity() ) ) cerr << getTime() << "RADIUS-PLUGIN: FOREGROUND THREAD: Waiting for new user." << endl;
             cout.flush();
 	    pthread_mutex_lock(context->getMutexSend());
-            while(context->UserWaitingtoAuth() == false) {
-                pthread_cond_wait(context->getCondSend(),context->getMutexSend());
-            }
+            pthread_cond_wait(context->getCondSend(),context->getMutexSend());
 	    pthread_mutex_unlock(context->getMutexSend());
         }
         if (context->getStopThread()==true)