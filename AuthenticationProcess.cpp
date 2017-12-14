b88400051f33ff3c2a8c07873310c523740e9819
Andrzej Shirinkin <andrej.shirinkin@gmail.com>
2017-12-14 17:09:45 +0200

    add some aux log

--- AuthenticationProcess.cpp
+++ AuthenticationProcess.cpp
@@ -42,7 +42,8 @@ void AuthenticationProcess::Authentication(PluginContext * context)
 {
 	UserAuth *		user; 		/**<The user to authenticate.*/
   	int 			command;	/**<A command from the parent process.*/
-  
+    int step = 0;
+
  	//Tell the parent everythink is ok.
   	try
   	{
@@ -70,67 +71,84 @@ void AuthenticationProcess::Authentication(PluginContext * context)
 		  	{
 			    user=new UserAuth;
 			    //get the user informations
+                step++;
 			    user->setUsername(context->authsocketforegr.recvStr());
-			    user->setPassword(context->authsocketforegr.recvStr());
-			    user->setDev(context->authsocketforegr.recvStr());
-			    user->setPortnumber(context->authsocketforegr.recvInt());
-			    user->setSessionId(context->authsocketforegr.recvStr());
-			    user->setCallingStationId(context->authsocketforegr.recvStr());
-			    user->setCommonname(context->authsocketforegr.recvStr());
+                step++;
+                user->setPassword(context->authsocketforegr.recvStr());
+                step++;
+                user->setDev(context->authsocketforegr.recvStr());
+                step++;
+                user->setPortnumber(context->authsocketforegr.recvInt());
+                step++;
+                user->setSessionId(context->authsocketforegr.recvStr());
+                step++;
+                user->setCallingStationId(context->authsocketforegr.recvStr());
+                step++;
+                user->setCommonname(context->authsocketforegr.recvStr());
 				// framed-ip is an @IP if we're renegotiating, "" otherwise
-			    user->setFramedIp(context->authsocketforegr.recvStr());
+                step++;
+                user->setFramedIp(context->authsocketforegr.recvStr());
 		 		
-			    if (DEBUG (context->getVerbosity()) && (user->getFramedIp().compare("") == 0))
+                if (DEBUG (context->getVerbosity()) && (user->getFramedIp().compare("") == 0))
 			    cerr << getTime() << "RADIUS-PLUGIN: BACKGROUND  AUTH: New user auth: username: " << user->getUsername() << ", password: *****, calling station: " << user->getCallingStationId() << ", commonname: " << user->getCommonname() << ".\n";
 
 			    if (DEBUG (context->getVerbosity()) && (user->getFramedIp().compare("") !=0 ))
 			    cerr << getTime() << "RADIUS-PLUGIN: BACKGROUND  AUTH: Old user ReAuth: username: " << user->getUsername() << ", password: *****, calling station: " << user->getCallingStationId() << ", commonname: " << user->getCommonname() << ".\n";
 			    
 			    //send the AcceptRequestPacket
-			    if (user->sendAcceptRequestPacket(context)==0) /* Succeeded */
+                step++;
+                if (user->sendAcceptRequestPacket(context)==0) /* Succeeded */
 			    {
 			     	//if the authentication succeeded
 			     	//create the user configuration file
 			     	//Unless this is a renegotiation (ie: if FramedIP is already set)
-			     	if (user->createCcdFile(context)>0 && (user->getFramedIp().compare("") == 0))
+                    step++;
+                    if (user->createCcdFile(context)>0 && (user->getFramedIp().compare("") == 0))
 			     	{
 			     		throw Exception ("RADIUS-PLUGIN: BACKGROUND AUTH: Ccd-file could not created for user with commonname: "+user->getCommonname()+"!\n");
 			     	}
 			     				     	
 			     	//tell the parent process
-			     	context->authsocketforegr.send(RESPONSE_SUCCEEDED);
+                    step++;
+                    context->authsocketforegr.send(RESPONSE_SUCCEEDED);
 								     	
 			     	//send the routes to the parent process
-			     	context->authsocketforegr.send(user->getFramedRoutes());
+                    step++;
+                    context->authsocketforegr.send(user->getFramedRoutes());
 					
 				//send the framed ip to the parent process
-			     	context->authsocketforegr.send(user->getFramedIp());
+                    step++;
+                    context->authsocketforegr.send(user->getFramedIp());
 										
 			     	//send the IPv6 routes to the parent process
-			     	context->authsocketforegr.send(user->getFramedRoutes6());
+                    step++;
+                    context->authsocketforegr.send(user->getFramedRoutes6());
 					
 				//send the framed IPv6 to the parent process
-			     	context->authsocketforegr.send(user->getFramedIp6());
+                    step++;
+                    context->authsocketforegr.send(user->getFramedIp6());
 										
 					//send the interval to the parent process
-			     	context->authsocketforegr.send(user->getAcctInterimInterval());
+                    step++;
+                    context->authsocketforegr.send(user->getAcctInterimInterval());
 			     	
 			     	//send the vsa buffer
-			     	context->authsocketforegr.send(user->getVsaBuf(), user->getVsaBufLen());
+                    step++;
+                    context->authsocketforegr.send(user->getVsaBuf(), user->getVsaBufLen());
 			     	
 			     	
 			     	//free user_context_auth
-			     	delete user;
+                    step++;
+                    delete user;
 			     	
 			     	if (DEBUG (context->getVerbosity()))
 		    			cerr << getTime() << "RADIUS-PLUGIN: BACKGROUND  AUTH: Auth succeeded in radius_server().\n";
-			  		
-			  		
 			    	
 			    }
 			    else /* Failed */
 			    {
-			    	context->authsocketforegr.send(RESPONSE_FAILED);
+                    step++;
+                    context->authsocketforegr.send(RESPONSE_FAILED);
 					throw Exception("RADIUS-PLUGIN: BACKGROUND  AUTH: Auth failed!.\n");	
 			    }
 		  	}
@@ -144,7 +162,7 @@ void AuthenticationProcess::Authentication(PluginContext * context)
 				}
 		    }
 		    catch(std::bad_alloc){
-		      cerr << getTime() << "RADIUS-PLUGIN: BACKGROUND AUTH: New failed for UserAuth." << endl;
+              cerr << getTime() << "RADIUS-PLUGIN: BACKGROUND AUTH: New failed for UserAuth. (step = "<<step<< ")" << endl;
 		      goto done;
 		    }
 		    catch (...)
